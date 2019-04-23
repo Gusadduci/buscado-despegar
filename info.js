bbf68498922aee271612cd884fd38ab1ab698d49
Gusadduci <47230345+Gusadduci@users.noreply.github.com>
2019-04-22 23:04:12 -0300

    Add files via upload

--- info.js
+++ info.js
@@ -1,39 +1,5 @@
 (function () {
-
-var datos = [
-{
-    lugar : 'Cancun',
-    fS : new Date(2019,3,19),
-    fV : new Date(2019,3,25),
-    salida : 'Lan',
-    imagen : 'Cancun-43.jpg'
-},
-{
-    lugar : 'Tucuman',
-    fS : new Date(2019,3,19),
-    fV : new Date(2019,8,13),
-    salida : 'Lan',
-    imagen : 'Cancun-43.jpg'
-},
-{
-    lugar : 'Mendoza',
-    fS : new Date(2019,3,19),
-    fV : new Date(2019,8,13),
-    salida : 'Ban',
-    imagen : 'Cancun-43.jpg'
-},
-{
-    lugar : 'Brazil',
-    fS : new Date(2019,3,19),
-    fV : new Date(2019,8,13),
-    salida :'Eze',
-    imagen : 'Cancun-43.jpg'
-}
-]
-    var resultados = document.getElementById('resultados');
     var boton = document.getElementById('buscar');
-    var result = '';
-    var numDatos = datos.length;
 
     var cero = function(){
         result = ' ';
@@ -41,77 +7,123 @@ var datos = [
     }
     
 
-    
-    var search = function(){
+        
         var fechaI = new Date(),
         dia = fechaI.getDate(),
         mes = fechaI.getMonth(),
         year = fechaI.getFullYear();
         var fecha = year/mes/dia;
-     
 
-        var partidaP = document.getElementById('partida');
-        var partida = partidaP.value;
+ 
+        var contador = 0
 
-        var destinoP = document.getElementById('destino');
-        var destino = destinoP.value;
-
-        var fS = document.getElementById('salida');
-        var salidaI = new Date(fS.value),
-            dia = salidaI.getDate(),
-            mes = salidaI.getMonth(),
-            year = salidaI.getFullYear();
-        var salida = year/mes/(dia+1);
-      
-
-        var fV = document.getElementById('vuelta');
-        var vueltaI = new Date(fV.value),
-            dia = vueltaI.getDate(),
-            mes = vueltaI.getMonth(),
-            year = vueltaI.getFullYear();
-        var vuelta = year/mes/(dia+1);
-     
+        var search = function(){
+            for (var i = 0; i < 1; i++){
+                var fS = document.getElementById('salida');
+                var salidaI = new Date(fS.value),
+                    dia = salidaI.getDate(),
+                    mes = salidaI.getMonth(),
+                    year = salidaI.getFullYear();
+                var salida = year/mes/(dia+1);
+              
         
-       
-        for (var i = 0 ; i < numDatos; i++){
+                var fV = document.getElementById('vuelta');
+                var vueltaI = new Date(fV.value),
+                    dia = vueltaI.getDate(),
+                    mes = vueltaI.getMonth(),
+                    year = vueltaI.getFullYear();
+                var vuelta = year/mes/(dia+1);
+                
+                
+                
+                var tiene_numeros = function(){
 
-            var fechaSalida = datos[i].fS,
-            dia = fechaSalida.getDate(),
-            mes = fechaSalida.getMonth(),
-            year = fechaSalida.getFullYear();
-            var fechaS = year/mes/dia;
-            console.log(fechaS)
-            var fechaVuelta = datos[i].fV,
-            dia = fechaVuelta.getDate(),
-            mes = fechaVuelta.getMonth(),
-            year = fechaVuelta.getFullYear();
-            var fechaV = year/mes/dia;
-            
-         
-             if (partida.length >= 3) {
-                if (destino.length >= 3){
-                    if (partida == datos[i].salida){
-                        if (destino == datos[i].lugar){
-                            if (salida <= fecha){
-                                if (vuelta <= fecha){
-                                    if(salida <= fechaS){
-                                        if (salida >= fechaV){
-                                            if(vuelta >= fechaV){
-                                                if(vuelta <= fechaS)
-                                                result = '<div id="lugares"><div class="image"></div><div class="info"><img src=" Cancun-43.jpg" >' + datos[i].lugar + '<div class="fechas" id="fechas">'+ datos[i].fS + ' ' + datos[i].fV + '</div></div></div>';
-                                                resultados.innerHTML = result;
-                                            }else { cero()}  
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    } 
+                    var er1 = new RegExp('[0123456789]')
+                    var rta1 = er1.test(partida)
+                    
+                    if (rta1 == true){
+                        contador = contador + 1;
+                    }else {contador = contador;
+                        document.getElementById('salida').className += 'error';}
+
+                    var rta2 = er1.test(destino)
+                
+
+                    if (rta2 == true){
+                        contador = contador + 1;
+                    }else {contador = contador;
+                        document.getElementById('salida').className += 'error';}
+
                 }
+      
+                var tres = function(){
+        
+                    if (partida.length == 3 ){
+                        {contador = contador;}
+                        
+                    }else {contador = contador + 1;
+                        document.getElementById('salida').className = 'error';}
+
+                    if (destino.length == 3 ){
+                        {contador = contador;}
+                        
+                    }else {contador = contador + 1;
+                        document.getElementById('salida').className = 'error';}
+                }
+
+                var verFecha = function(){
+        
+                    if (salida <= fecha){
+                        {contador = contador;}
+                        
+                    }else {contador = contador + 1;
+                           document.getElementById('salida').className = 'error'; }
+
+                    if (vuelta <= fecha){
+                        {contador = contador;}
+                        
+                    }else {contador = contador + 1;
+                        document.getElementById('vuelta').className = 'error';}
+
+                    if (vuelta <= salida){
+                        {contador = contador;}
+                        
+                    }else {contador = contador + 1;
+                        document.getElementById('vuelta').className = 'error';
+                        document.getElementById('salida').className = 'error';}
+                }
+        
+                
+                var partidaP = document.getElementById('partida');
+                var partida = partidaP.value;
+                var destinoP = document.getElementById('destino');
+                var destino = destinoP.value;
+
+                var resultados = document.getElementById('resultados');
+                var result = ' ';
+
+                tiene_numeros()
+                tres()
+                verFecha()
+                
+                
+
+                if (contador == 0){
+                    result = '<div class="texto">' + ' Tu lugar de partida es en '+ partida + ' el dia ' + fS.value  + ' vas hasta ' + destino + ' y vuelves el ' + fV.value +  '</div>';
+                    resultados.innerHTML = result;
+
+                }else{
+                    contador = 0;
+                }
+                
+                
             }
+
         }
 
+    
+    
     boton.addEventListener("click", search);
+    
 
 }())
